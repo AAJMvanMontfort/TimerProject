d104774748b696b0b0aab926985795c1b4ee8681
AAJMvanMontfort <lout@me.com>
2020-07-05 14:47:51 +0200

    Initial Commit

---
+++ TimerProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerProject
+//
+//  Created by AAJM van Montfort on 05/07/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
